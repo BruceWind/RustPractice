6762e88f192d790f0f7c7568d4e49a3a94746ee5
bruce <hide@hide.com>
2023-01-18 11:13:43 +0800

    [feat]:{an example for hiding string from decompiling.}

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # RustPractice
 
 A repo, used for learning and practice.
+
+## Some examples explained in this repo:
+1. hide your secret strings or keys for decompiling.
+
+# Run
+### requirements in advance of running.
+  1. export LITCRYPT_ENCRYPT_KEY="brucewind20230000"
+  2. cargo build
+  3. cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate litcrypt; //#litcrypt in there is used to hide string from decompiling.
+
+use_litcrypt!();
+
 fn main() {
-    println!("Hello, world!");
+    // This string give you a example for a case that a string could be get by decompling.
+    // In this case, you can try run with string command to look into executable file, e.g:
+    //        strings ./target/debug/RustPractice | grep exposure
+    println!("exposure string.");
+    // On the other hand, this string is another example for hiding from decompiling.
+    //      also you can use the last command shown above to filter "hidden-string" with `grep hidden`.
+    println!("{}", lc!("hidden-string."));
+
+
 }
+
+
+
+
